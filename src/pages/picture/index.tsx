cb51d9b2b5e1534704537ed4d364d23954a66d46
render-ydb <yangdaobo.render@gmail.com>
2024-04-28 18:47:22 +0800

    feat: 修改picture地址

--- src/pages/picture/index.tsx
+++ src/pages/picture/index.tsx
@@ -5,19 +5,9 @@ import styles from './index.module.css';
 const App = () => {
   return (
     <View className={styles.container}>
-      <Picture
-        className={styles.img}
-        src="https://k.sinaimg.cn/n/sinacn10101/50/w640h1010/20200108/4311-imvsvyz6534073.jpg/w700d1q75cms.jpg"
-      ></Picture>
-      <Picture
-        className={styles.img}
-        src="https://gd-hbimg.huaban.com/32f1e7c97b1c9d56f9b49d92abe48b317a7caa7cb2cf-yXpHpy_fw658"
-      ></Picture>
-      <Picture
-        className={styles.img}
-        lazyload
-        src="https://scpic.chinaz.net/Files/pic/pic9/201410/apic7268.jpg"
-      ></Picture>
+      <Picture className={styles.img} src={'/images/picture1.jpg'} />
+      <Picture className={styles.img} src={'/images/picture2.jpg'} />
+      <Picture className={styles.img} lazyload src={'/images/picture3.jpg'} />
     </View>
   );
 };
